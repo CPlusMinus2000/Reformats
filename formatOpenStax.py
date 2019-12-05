3955aeb5015dc418234f3dcafa922abfbe4b8f74
Colin He <hcolin88@gmail.com>
2019-12-05 09:27:01 -0500

    Merged figureArranger and tooltipCreator into one program

--- formatOpenStax.py
+++ formatOpenStax.py
@@ -6,6 +6,11 @@ formatted = '<span class="OpenStaxChem-term">TERM'
 formatted += '<span class="OpenStaxChem-definition">'
 formatted += '<span class="OpenStaxChem-t">TERM</span>DEFINITION</span></span>'
 
+template = ('<figure>'
+            '<img alt="ALTTEXT" class="center" data-media-type="image/jpg"'
+            ' src="LINK" /><figcaption><span class="OpenStaxChem-fig-label">'
+            'Figure NUMBER</span>CAPTION</figcaption></figure>')
+
 # Path of manifest and any pieces of text that should go into MathML
 source = "../../OpenStax Backups/Chemistry/manifest.xml"
 mathify = ["&#960;", "&#963;"]
@@ -82,7 +87,7 @@ def makeTooltip(text: str) -> str:
     return filled.replace("DEFINITION", definition)
 
 # Combs the text in the clipboard for keywords, making them into tooltips.
-def main() -> None:
+def addTooltips() -> None:
     txt = pyp.paste()
 
     # Make four arrays for outer and inner start and end indices of keywords.
@@ -101,5 +106,48 @@ def main() -> None:
     # Copy the formatted text.
     pyp.copy(txt)
 
+# Makes a new, properly formatted figure given the old figure passed in as text.
+#  Note: the abundance of +1's and -2's in this function is due to the fact
+#  that quotation marks will be present in both the old figure and the template
+#  figure, so I need to get rid of them from the old figure using +1/-2's.
+def makeFigure(text: str) -> str:
+    # Get the caption
+    capStart = text.find("</span>") + len("</span>")
+    caption = text[capStart:text.find("</figcaption>")]
+    
+    # Get the alt text
+    altLoc = text.find("img alt=") + len("img alt=") + 1
+    altEnd = text.find("data-media-type=") - 2 # Slice off the last quotation
+    altText = text[altLoc:altEnd]
+
+    # Get the image link
+    linkLoc = text.find("src=") + len("src=") + 1
+    linkEnd = text.find("/>", linkLoc) - 2
+    link = text[linkLoc:linkEnd]
+
+    # Construct the new figure and return
+    figure = template.replace("ALTTEXT", altText)
+    figure = figure.replace("LINK", link)
+    return figure.replace("CAPTION", caption)
+
+# Parses the clipboard text for all old figures, and passes them incrementally
+#  back into the clipboard for pasting.
+def addFigures() -> None:
+    txt = pyp.paste()
+
+    # Make four arrays for outer and inner start and end indices of keywords.
+    sIndices = findAll(txt, '<figure')
+    eIndices = [txt.find('</figure>', i) + len('</figure>') for i in sIndices]
+    
+    # Replace all of the keywords with their corresponding tooltips, making
+    #  sure to move backwards so as not to mess up other indices.
+    for i in reversed(range(len(sIndices))):
+        oldFigure = txt[sIndices[i]:eIndices[i]]
+        txt = txt.replace(oldFigure, makeFigure(oldFigure), 1)
+    
+    # Copy the formatted text.
+    pyp.copy(txt)
+
 if __name__ == "__main__":
-    main()
+    addTooltips()
+    addTooltips()
