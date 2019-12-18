7f4b644666a97b26724025073232cd9ff514e1a6
Colin He <hcolin88@gmail.com>
2019-12-18 10:13:30 -0500

    A new tag appears

--- addTags.py
+++ addTags.py
@@ -1,7 +1,7 @@
 # Short script that adds markup tags to highlighted text
 import pyperclip as pyp
 
-mode = 5
+mode = 6
 
 # List of Modes:
 #  1: Calculator code span environment
@@ -9,13 +9,15 @@ mode = 5
 #  3: 70% fontsize for limit notation
 #  4: Basic MathML tags (mn)
 #  5: Span labels (specifically for OpenStaxChem)
+#  6: Font-resizing spans (for Wisconsin S2019)
 
 tD1 = {
     1: '<span class="OpenStaxIS-code">',
     2: '<menclose notation="updiagonalstrike">\n',
     3: '<mstyle fontsize="0.7em">\n',
     4: '<math xmlns="http://www.w3.org/1998/Math/MathML">\n<mrow>\n<mn>\n',
-    5: '<span class="OpenStaxChem-label">'
+    5: '<span class="OpenStaxChem-label">',
+    6: '<span style="font-size:16px;">'
 }
 
 tD2 = {
@@ -24,6 +26,7 @@ tD2 = {
     3: '\n</mstyle>',
     4: '\n</mn>\n</mrow>\n</math>',
     5: '</span>'
+    6: '</span>'
 }
 
 pyp.copy(tD1[mode] + pyp.paste() + tD2[mode])
